3f2abaa33f73e9e3bfd4a1d49f1ed56b1d7f8759
ramonromeroyvigil <rjryvgit@gmail.com>
2014-12-25 00:12:18 -0500

    added addition operator.

--- src/main/scala/sgfParser/Board/BoardSpec.scala
+++ src/main/scala/sgfParser/Board/BoardSpec.scala
@@ -2,16 +2,21 @@ package sgfParser.Board
 
 trait BoardSpec {
   
-  final val WHITE_OCCUPIED : Byte = -1
-  final val BLACK_OCCUPIED : Byte = 1
-  final val EMPTY          : Byte = 0
-  
   def dimension : BoardDimension
   
   def board : BoardRepr
   
   def generateEmptyBoard(dim: BoardDimension): BoardRepr = Array.ofDim[Byte](dim._1*dim._2)
 
+  def +(move : Move) : Option[BoardSpec] = 
+    if (move.position._1.toInt * move.position._2.toInt > board.length || 
+        board(posToIndex(move.position)) != EMPTY) 
+      None
+    else 
+      Some(new StandardBoard(dimension, board.updated(posToIndex(move.position), move.side)))
+  
+  def posToIndex(position : Position) = 
+    position._1.toInt*dimension._1.toInt + position._2.toInt
 }//end trait BoardSpec
 
 //31337
