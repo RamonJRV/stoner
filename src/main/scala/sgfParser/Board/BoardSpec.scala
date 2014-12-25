480b0718706ce574a7b371dfd15dac920a3d101a
ramonromeroyvigil <rjryvgit@gmail.com>
2014-12-24 23:24:44 -0500

    first checkin of board src files.

---
+++ src/main/scala/sgfParser/Board/BoardSpec.scala
@@ -0,0 +1,15 @@
+package sgfParser.Board
+
+trait BoardSpec {
+  
+  final val WHITE_OCCUPIED : Byte = -1
+  final val BLACK_OCCUPIED : Byte = 1
+  final val EMPTY          : Byte = 0
+  
+  def dimension : BoardDimension
+  
+  def board : BoardRepr
+  
+  def generateEmptyBoard(dim: BoardDimension): BoardRepr = Array.ofDim[Byte](dim._1*dim._2)
+
+}//end trait BoardSpec
