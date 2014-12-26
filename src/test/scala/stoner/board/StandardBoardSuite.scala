0ac64dd1e0be52fc55e7ffae725bde982d20fe58
ramonromeroyvigil <rjryvgit@gmail.com>
2014-12-25 19:55:52 -0500

    updated board unit testing.

--- src/test/scala/stoner/board/StandardBoardSuite.scala
+++ src/test/scala/stoner/board/StandardBoardSuite.scala
@@ -8,14 +8,45 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class StandardBoardSuite extends FunSuite {
+  val blackCornerPos : Position = (0,0)
+  val blackCornerMove : Move = Move(BLACK, blackCornerPos)
+  
+  val whiteCornerPos : Position = ((STANDARD_COLUMN - 1).toByte,0.toByte)
+  val whiteCornerMove : Move = Move(WHITE, whiteCornerPos)
+  
+  val emptyStandardBoard = new StandardBoard
   
   test("New standard board is correct size") {
-    val testBoard = new StandardBoard
-    assert(testBoard.board.length == STANDARD_COLUMN.toInt * STANDARD_ROW.toInt)
+    assert(emptyStandardBoard.board.length == STANDARD_COLUMN.toInt * STANDARD_ROW.toInt)
   }
   
   test("New Board has no positions") {
-    assert((new StandardBoard).board.sum == 0)
+    assert(emptyStandardBoard.board.sum == 0)
+  }
+  
+  test("Single Move creates Single Stone Position") {
+    
+    var testBoard : BoardSpec = emptyStandardBoard + blackCornerMove
+    assert(testBoard.board.sum == BLACK.toInt)
+    assert(testBoard.board.filter(_ == BLACK).length == 1)
+    assert(testBoard.getPosValue(blackCornerPos) == BLACK)
+    
+    testBoard = emptyStandardBoard + whiteCornerMove
+    assert(testBoard.board.sum == WHITE.toInt)
+    assert(testBoard.board.filter(_ == WHITE).length == 1)
+    assert(testBoard.getPosValue(whiteCornerPos) == WHITE)
+    
+  }
+  
+  test("Two moves creates Two Stone Position") {
+    val testBoard = emptyStandardBoard + blackCornerMove + whiteCornerMove
+    
+    assert(testBoard.board.sum == 0)
+    assert(testBoard.board.filter(_ == BLACK).length == 1)
+    assert(testBoard.board.filter(_ == WHITE).length == 1)
+    
+    assert(testBoard.getPosValue(blackCornerPos) == BLACK)
+    assert(testBoard.getPosValue(whiteCornerPos) == WHITE)
   }
 
 }//end object BoardAndMoveSuite extends FunSuite
