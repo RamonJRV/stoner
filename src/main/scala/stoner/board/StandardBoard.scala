027738c0b7299174c15ed9b21ab1d3b55e542b4d
ramonromeroyvigil <rjryvgit@gmail.com>
2014-12-26 22:40:02 -0500

    changed to grid representation and removed case class.

--- src/main/scala/stoner/board/StandardBoard.scala
+++ src/main/scala/stoner/board/StandardBoard.scala
@@ -1,12 +1,14 @@
  package stoner.board
 
 
-case class StandardBoard(grid : GridRepr = new Array[Byte](STANDARD_POINT_COUNT)) extends Board {
+class StandardBoard(grid_i : GridRepr = new Array[Byte](STANDARD_POINT_COUNT)) extends Board {
   
+  override val grid = grid_i
+   
   override def dimension : BoardDimension = (STANDARD_COLUMN,STANDARD_ROW)
 
   override def +(move : Move) : Board = 
-    StandardBoard(grid.updated(posToIndex(move.position), move.side))
+    new StandardBoard(grid.updated(posToIndex(move.position), move.side))
 }
 
 //31337
