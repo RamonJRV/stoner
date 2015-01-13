11b42ab83f82adec31c1cd69f6017fee6fc54d85
ramonromeroyvigil <rjryvgit@gmail.com>
2015-01-12 20:28:55 -0500

    started test suite for sgf parser.

--- src/test/scala/stoner/sgfParser/sgfParserSuite.scala
+++ src/test/scala/stoner/sgfParser/sgfParserSuite.scala
@@ -1,10 +1,20 @@
 package stoner.sgfParser
 
+import scala.io.Source
+
 import org.scalatest.FunSuite
 
-class sgfParserSuite {
-  
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
+class sgfParserSuite extends FunSuite {
+  
+  val testList = Source.fromFile("/Users/ramonromeroyvigil/Documents/experimentalData/go/GoGoD/Database/0196-1699/1698-12-21a.sgf").toList
+
+  test("Test List is Valid") {
+    assert(testList.size > 0)
+  }//end test("Test List is Valid")
 }//end class sgfParserSuite
 
 //31337
