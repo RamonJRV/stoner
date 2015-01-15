f2e0235b26d07219abe08934ce9ac16141efc7da
ramonromeroyvigil <rjryvgit@gmail.com>
2015-01-14 21:23:57 -0500

    fixed one bug and found another.

--- src/main/scala/stoner/sgfParser/SGFStringParser.scala
+++ src/main/scala/stoner/sgfParser/SGFStringParser.scala
@@ -18,18 +18,23 @@ object SGFStringParser {
         
         if(line.startsWith("RE")) {
           if(line(3) == 'B') winner_o = BLACK
-          else winner_o = WHITE
-        }
+          else if(line(3) == 'W') winner_o = WHITE
+        }//end if(line.startsWith("RE"))
         else if(line.startsWith("AB")) {
           for(s <- line.drop(1).sliding(2, 4)) {
             st_o.append(PosFlip(PositionTranslator.strRepToPos(s), BLACK))
           }
-        }
+        }//end else if(line.startsWith("AB"))
+        else if(line.startsWith("AW")) {
+          for(s <- line.drop(1).sliding(2, 4)) {
+            st_o.append(PosFlip(PositionTranslator.strRepToPos(s), WHITE))
+          }
+        }//end else if(line.startsWith("AW"))
         else if(line.startsWith(";")) {
           val moves = line.split(';').filter(_.size > 0)
           st_o ++= moves.map((m : String) => Move(PositionTranslator.strRepToPos(m.substring(2,4)),
                                                   sideMap(m(0))))
-        }
+        }//end else if(line.startsWith(";"))
       }//end for(line <- lines.tail)
       
       Some(Game(new Board(st_o), winner_o))
--- src/test/scala/stoner/sgfParser/sgfParserSuite.scala
+++ src/test/scala/stoner/sgfParser/sgfParserSuite.scala
@@ -7,12 +7,20 @@ import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
+import stoner.board.EMPTY 
+
 @RunWith(classOf[JUnitRunner])
 class sgfParserSuite extends FunSuite {
   
-  println(System.getProperty("user.dir"))
-  val testList = Source.fromFile("src/main/resources/GoGoD/Database/0196-1699/1698-12-21a.sgf").getLines.toList
-
+  def filenameToLines(f: String) = Source.fromFile(f).getLines.toList
+  
+  val topDir = "src/main/resources/GoGoD/Database/"
+  
+  val testList = filenameToLines(topDir + "0196-1699/1698-12-21a.sgf")
+    
+  val testListNoWinner = filenameToLines(topDir + "0196-1699/0196-00-00.sgf")
+    
+  
   test("Test List is Valid") {
     assert(testList.size > 0)
     
@@ -23,7 +31,15 @@ class sgfParserSuite extends FunSuite {
     val game = SGFStringParser.parseLines(testList)
     
     assert(game.isDefined)
-  }
+  }//end test("Game Parsing of test list")
+  
+  test("Game Parsing of Unknown Winner") {
+    val game = SGFStringParser.parseLines(testListNoWinner)
+    
+    assert(game.isDefined)
+    assert(game.get.winner == EMPTY)
+  }//end test("Game Parsing of Unknown Winner")
+  
 }//end class sgfParserSuite
 
 //31337
