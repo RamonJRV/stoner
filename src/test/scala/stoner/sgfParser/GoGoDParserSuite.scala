914b06a2b6f2f6570c803f41a092acc8dfec6bba
ramonromeroyvigil <rjryvgit@gmail.com>
2015-03-14 13:47:41 -0400

    added another year of parsing to the test suite.

--- src/test/scala/stoner/sgfParser/GoGoDParserSuite.scala
+++ src/test/scala/stoner/sgfParser/GoGoDParserSuite.scala
@@ -6,21 +6,29 @@ import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
+import java.io.File
+
 @RunWith(classOf[JUnitRunner])
 class GoGoDParserSuite extends FunSuite {
-  
+
   test("Recursive List files works") {
-    
+
     val files = GoGoDParser.recursiveListFiles()
-    
+
     assert(files.length > 0)
   }//end test("Recursive List files")
-  
-  test("getGamesFromSGFFiles works") {
+	
+  test("getGamesFromSGFFiles works on 2007") {
     val games = GoGoDParser.getGamesFromSGFFile(GoGoDParser.GO_GO_D_DIR + "2007")
     
     assert(games.length > 0)
   }
+    
+  test("getGamesFromSGFFiles works on 2013") {
+    val games = GoGoDParser.getGamesFromSGFFile(GoGoDParser.GO_GO_D_DIR + "2013")
+    
+    assert(games.length > 0)
+  }//end test("getGamesFromSGFFiles works on 2007")
 }//end class GoGoDParserSuite extends FunSuite
 
 //31337
