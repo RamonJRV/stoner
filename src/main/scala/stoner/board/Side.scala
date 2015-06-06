044d3549b2689c60e0d9c4c6b87f26909045a3e9
ramonromeroyvigil <rjryvgit@gmail.com>
2015-06-06 11:09:50 -0400

    Switched Side objects to be final.

--- src/main/scala/stoner/board/Side.scala
+++ src/main/scala/stoner/board/Side.scala
@@ -10,7 +10,7 @@ sealed trait Side {
   
 }//end trait Side
 
-case object WHITE extends Side {
+final case object WHITE extends Side {
   override val opposite = BLACK
   
   override val toChar = 'W'
@@ -18,14 +18,14 @@ case object WHITE extends Side {
   
 }//end case object WHITE extends Side
 
-case object BLACK extends Side {
+final case object BLACK extends Side {
   override val opposite = WHITE
   
   override val toChar = 'B'
   override val toInt = 1
 }//end case object BLACK extends Side
 
-case object EMPTY extends Side {
+final case object EMPTY extends Side {
   override val opposite = EMPTY
   
   override val toChar = '*'
