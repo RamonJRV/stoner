892255abce74e31b7532c65255f004b54aacb7ac
ramonromeroyvigil <rjryvgit@gmail.com>
2015-07-12 17:40:24 -0400

    added companion Position object and label to position method.

--- src/main/scala/stoner/board/Position.scala
+++ src/main/scala/stoner/board/Position.scala
@@ -2,6 +2,17 @@ package stoner.board
 
 import stoner.board.BoardDimension.STANDARD_BOARD_DIM
 
+object Position {
+  def labelClassToPos(label : Double, boardDim : BoardDimension) = {
+    val p = label.toInt
+    
+    val c = p / boardDim.row
+    val r = p - c*boardDim.row
+    
+    Position(c,r)
+  }
+}
+
 case class Position(column : Int, row : Int) {
   
   def toLabelClass(boardDim : BoardDimension = STANDARD_BOARD_DIM) =
