7214dee1018e66018347566ba0319d0e3504cc90
ramonromeroyvigil <rjryvgit@gmail.com>
2015-07-12 13:59:52 -0400

    added a method to Position that converts it to a label.

--- src/main/scala/stoner/board/Position.scala
+++ src/main/scala/stoner/board/Position.scala
@@ -1,5 +1,12 @@
 package stoner.board
 
-case class Position(column : Int, row : Int)
+import stoner.board.BoardDimension.STANDARD_BOARD_DIM
+
+case class Position(column : Int, row : Int) {
+  
+  def toLabelClass(boardDim : BoardDimension = STANDARD_BOARD_DIM) =
+    (column*boardDim.row + row).toDouble
+  
+}//end case class Position(column : Int, row : Int)
 
 //31337
