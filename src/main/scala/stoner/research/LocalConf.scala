82cf16d21bafdd38f4574bf4165aae5e30e41c93
ramonromeroyvigil <rjryvgit@gmail.com>
2015-07-12 11:24:18 -0400

    added a local spark context creator.

--- src/main/scala/stoner/research/LocalConf.scala
+++ src/main/scala/stoner/research/LocalConf.scala
@@ -1,6 +1,6 @@
 package stoner.research
 
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, SparkContext}
 
 /**
  * Helper functions for creating spark configurations on the local host.
@@ -16,6 +16,8 @@ object LocalConf {
   def createConf(name : String) = new SparkConf().setMaster(localMaster)
                                                  .setAppName(name)
   
+  def createContext = new SparkContext(createConf)
+                                                 
 }//end object LocalConf
 
 //31337
