ad5a647461b6d94fbaf1ebdb49a56d80ac96191c
ramonromeroyvigil <rjryvgit@gmail.com>
2015-08-09 00:03:31 -0400

    Added package file for highest level package.

---
+++ src/main/scala/stoner/package.scala
@@ -0,0 +1,21 @@
+
+/**
+ * For a description of stoner see the src/README.md file and the project
+ * wiki.
+ * 
+ * The sub packages are organized as follows:
+ * 
+ *   board     : Internal representations of a game and game history.
+ *   sgfParser : Parses .sgf files into representations contained within the 
+ *               board package.
+ *   translate : Converts representations with the board package into data 
+ *               structures that facilitate machine learning.
+ *   research  : Where all of the magic happens.
+ *   ai        : AI player logic.
+ *   
+ */
+package object stoner {
+
+}//end package object stoner
+
+//31337
